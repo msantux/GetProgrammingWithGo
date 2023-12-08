cf9a22135f601b475dc7468e88403012eaed12e1
msantux <marcelo@santux.com>
2023-12-08 15:58:08 -0300

    renamed:    Chapter_02/mars.go -> Chapter_02/01_Performing_Calculations/mars.go 	new file:   Chapter_02/02_Formatted_Print/fmt.go

--- Chapter_02/01_Performing_Calculations/mars.go
+++ Chapter_02/01_Performing_Calculations/mars.go
@@ -5,8 +5,8 @@ import "fmt"
 // main is the function where it all begins.
 func main() {
 	fmt.Print("My weight on the surface of Mars is ")
-	fmt.Print(149.0 * 0.3783)
-	fmt.Print(" lbs, and I would be ")
-	fmt.Print(41 * 365 / 687)
+	fmt.Print(79 * 0.3783)
+	fmt.Print(" kgs, and I would be ")
+	fmt.Print(50 * 365 / 687)
 	fmt.Print(" years old")
 }
---
+++ Chapter_02/02_Formatted_Print/fmt.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// main is the function where it all begins.
+func main() {
+	fmt.Printf("My weight on the surface of Mars is %v lbs,", 79*0.3783)
+	fmt.Printf(" and I would be %v years old.\n", 50*365/687)
+}
