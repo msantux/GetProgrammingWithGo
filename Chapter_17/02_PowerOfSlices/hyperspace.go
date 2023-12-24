32bcf3c5d233c1ff797b7963711657037f2ac0cc
msantux <marcelo@santux.com>
2023-12-24 13:29:37 -0300

    new file:   Chapter_17/01_SlicingAnArray/slicing.go 	new file:   Chapter_17/02_PowerOfSlices/hyperspace.go 	new file:   Chapter_17/03_SlicesWithMethods/sort.go 	new file:   Chapter_17/04_Experiment/terraform.go

---
+++ Chapter_17/02_PowerOfSlices/hyperspace.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// hperspace removes the space surrounding worlds
+func hyperspace(worlds []string) {
+	for i := range worlds {
+		worlds[i] = strings.TrimSpace(worlds[i])
+	}
+}
+
+func main() {
+	planets := []string{
+		" Venus   ",
+		"Earth ",
+		" Mars",
+	}
+	hyperspace(planets)
+
+	fmt.Println(strings.Join(planets, ""))
+}
